2f55b87fcd4c69334fb33bc1c51f56370c99963e
Jestin64 <jestin@xtravision.ai>
2023-04-15 11:09:25 +0530

    setup of express server+middleware

---
+++ config/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || "4000",
+//   jwtSecretKey: process.env.JWT_SECRET || "your_secret_key",
+//   mongoUri: process.env.MONGODB_URI,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,34 @@
-import express from 'express';
-
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import cors from "cors";
+import helmet from "helmet";
+import { config } from "../config/config";
+import { createServer } from "http";
 
 const app = express();
-const PORT = process.env.PORT || '3000';
+const PORT = config.port || "4000";
 
-app.get('/', (req,res)=>{
-    res.send('hello world!')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
 
-app.listen(PORT, ()=>{
-    console.log('listening at port: ', PORT); 
+app.get("/", (req, res) => {
+  res.send("hello world!");
 });
+
+const setupServer = () => {
+  return new Promise<void>((resolve) => {
+    const server = createServer(app);
+    server.listen(PORT, () => {
+      resolve();
+      console.log("listening at port: ", PORT);
+    });
+  });
+};
+
+setupServer();
